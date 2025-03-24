12204f27db626dbcaaf1853446ffbcf3aad47946
Benjamin Logan Casinelli <146365093+casibe01@users.noreply.github.com>
2025-03-24 11:24:17 -0400

    Add files via upload

---
+++ db_connect.php
@@ -0,0 +1,20 @@
+<?php
+	$server = 'cray.cs.gettysburg.edu';
+	$dbase = 's25_casibe01';
+	$user = 'casibe01';
+	$pass = 'casibe01';
+	$dsn = "mysql:host=$server;dbname=$dbase";
+	
+	try{
+		$db = new PDO($dsn, $user, $pass);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+		print "<H3>Successfully connected to database.</H3>\n";
+	}
+	catch(PDOException $e){
+		error_log($e->getMessage());
+		print "<H3>ERROR connecting to database</H3>\n";
+		//print $e->getMessage();
+		exit();
+	}
+?>
