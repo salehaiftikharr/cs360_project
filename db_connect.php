9e3dee01195d653eebb7224e8f5c35331a1255f5
Benjamin Logan Casinelli <146365093+casibe01@users.noreply.github.com>
2025-03-24 11:25:12 -0400

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php
 	$server = 'cray.cs.gettysburg.edu';
-	$dbase = 's25_casibe01';
+	$dbase = 's25_bcs';
 	$user = 'casibe01';
 	$pass = 'casibe01';
 	$dsn = "mysql:host=$server;dbname=$dbase";
@@ -9,11 +9,11 @@
 		$db = new PDO($dsn, $user, $pass);
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-		print "<H3>Successfully connected to database.</H3>\n";
+		//print "<H3>Successfully connected to database.</H3>\n";
 	}
 	catch(PDOException $e){
 		error_log($e->getMessage());
-		print "<H3>ERROR connecting to database</H3>\n";
+		//print "<H3>ERROR connecting to database</H3>\n";
 		//print $e->getMessage();
 		exit();
 	}
