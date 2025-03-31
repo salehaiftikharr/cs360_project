181015dd8414a254256d0c84b44cf7525cd8c42b
Benjamin Logan Casinelli <146365093+casibe01@users.noreply.github.com>
2025-03-31 13:33:42 -0400

    Update profile.php

--- views/profile.php
+++ views/profile.php
@@ -4,10 +4,13 @@ session_start();
 
 <!DOCTYPE html>
 <HTML>
-<HEAD>
-<TITLE> Profile Page </TITLE>
-</HEAD>
-<BODY>
-</BODY>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<title> Profile Page </title>
+</head>
+<body>
+</body>
 
 </HTML>
