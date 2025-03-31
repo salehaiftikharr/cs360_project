a09eb349b0b00c71e92e144d74a1a96b3c65874b
Benjamin Logan Casinelli <146365093+casibe01@users.noreply.github.com>
2025-03-31 13:37:20 -0400

    Update index.php

--- views/index.php
+++ views/index.php
@@ -1,4 +1,6 @@
-
+<?php 
+session_start();
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <?php include("./bootstrap.php"); ?>
     <title>Fuerza</title>
 
 </head>
