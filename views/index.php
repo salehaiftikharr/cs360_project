74c786237395c10a777d89aa28b8d3fb2b8ecdac
Benjamin Logan Casinelli <146365093+casibe01@users.noreply.github.com>
2025-03-31 13:36:34 -0400

    Update index.php

--- views/index.php
+++ views/index.php
@@ -1,1 +1,72 @@
 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Fuerza</title>
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-md">
+        <div class="container">
+            <a class="navbar-brand" href="#">Fuerza</a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="product.html">Product</a></li>
+                    <li class="nav-item"><a class="nav-link" href="mission.html">Mission</a></li>
+                    <li class="nav-item"><a class="nav-link" href="new.html">What's New</a></li>
+                    <li class="nav-item"><a class="nav-link" href="login.html">Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="main-content">
+        <h2>Connect and Grow</h2>
+        <br>
+        <p>Join your friends and build a community centered around health, fitness and self-improvement.</p>
+        <br>
+        <div class ="main-pic">
+        <img src="img/lifting_class.png">
+        </div>
+    </div>
+
+
+    <h5>Features</h5>
+
+        <div class="features">
+            <div class="fcontainer">
+                <div class="gallery">
+                    <img src="img/spotting.png">                 
+                </div>
+                <div class="gallery">
+                    <img src="img/measure.png">
+                 
+                </div>
+                <div class="gallery">
+                    <img src="img/coach.png">
+                </div>
+                <div>
+                    <h6>Connect With Friends</h6>
+                    <p>Fuerza allows you to share and track workouts with your favorite people, as well as find others on a similar journey.</p>
+                </div>
+                <div>
+                    <h6>Track Your Progress</h6>
+                    <p>See how you've improved using our analysis features.</p>
+                </div>
+                <div>
+                    <h6>Learn From Others</h6>
+                    <p>Gain unique perspectives on fitness through collaboration and community.</p>
+                </div>
+            </div>
+        </div>
+
+    <footer>
+        <p>© 2025 Fuerza Inc.</p>
+    </footer>
+</body>
+</html>
