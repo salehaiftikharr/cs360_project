8e16d64b3db23f66a9b975b766db479a02b90bef
Benjamin Logan Casinelli <146365093+casibe01@users.noreply.github.com>
2025-04-09 17:42:52 -0400

    Update login.php

--- views/login.php
+++ views/login.php
@@ -1,9 +1,9 @@
 <?php 
 
 session_start();
-
 include_once("../whitco_util.php"); 
-include_once("../dbproj_connect.php"); 
+include_once("../casibe_util.php"); 
+include("./bootstrap.php");
 $menu = "notLoggedIn";
 ?>
 
@@ -13,8 +13,14 @@ $menu = "notLoggedIn";
     <title>Login</title>
 </head>
 <body>
+    <?php genHeader(); ?>
+ <div class="main-content">
     <h2>Login Here:</h2>
     <?php
+    
+    $unam = $_SESSION['uname'];
+    print"<p>$unam</p>";
+    
     switch($menu){
         case 'notLoggedIn':
             genLoginForm();
@@ -24,7 +30,12 @@ $menu = "notLoggedIn";
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         processLogin($db, $_POST);
     }
+    
     ?>
     <p>Not registered? <a href="signup.php">Click here to sign up!</a></p>
+    </div>
+    
+    
+    <?php genFooter(); ?>
 </body>
 </html>
