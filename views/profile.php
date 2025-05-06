c47be0365b7f9d2f3096aec814b0457f9db56c68
Benjamin Logan Casinelli <146365093+casibe01@users.noreply.github.com>
2025-05-06 13:48:48 -0400

    Update profile.php

--- views/profile.php
+++ views/profile.php
@@ -1,6 +1,5 @@
 <?php 
 session_start();
-//var_dump($_SESSION);
 include_once("../whitco_util.php"); 
 include_once("../casibe_util.php"); 
 
@@ -12,6 +11,8 @@ if(!$_SESSION["valid"]){
 if(isset($_GET['account'])){
 	$account = $_GET['account'];
 }
+
+
 else{
     header("Location:" . "profile.php?account=" . $_SESSION['uname']);
 }
@@ -24,10 +25,24 @@ else{
 
 <title> Profile Page </title>
 <?php
-	include("./bootstrap.php");
+	include("../bootstrap.php");
 	$uid = $_SESSION['uid'];
 	$str = "SELECT uid FROM User WHERE username = '$account'";
 	$pid = $db->query($str)->fetch()['uid']; // PROFILE ID!!!
+	
+	if($_GET['menu'] == "follow"){
+		$str = "INSERT INTO Follows (follower_uid, following_uid) VALUES ($uid, $pid)";
+		$db->query($str);
+		header("refresh:0.2;url=profile.php?account=$account");
+	}
+	
+	
+	if($_GET['menu'] == "unfollow"){
+		$str = "DELETE FROM Follows WHERE follower_uid = $uid AND following_uid = $pid";
+		$db->query($str);
+		header("refresh:0.2;url=profile.php?account=$account");	
+	}
+
 
 ?>
     
@@ -49,10 +64,29 @@ else{
 			print'<IMG src="'.$image.'" class="pfp-image">';
 			print"<div class='profileDiv'><H4>$name</H4>";
 			print"<H6>$username</H6>$bio</div>";
-		?>
-		<DIV class="col-4 menuItem"><A href="?menu=follow">Follow</A></DIV>
-		</div>
-		<?php genPosts($db, $pid);
+		
+		
+		if($uid != $pid){
+			
+			$str = "SELECT * FROM Follows WHERE follower_uid = $uid AND following_uid = $pid";
+			$res = $db->query($str)->fetch();
+			
+			if(!empty($res)){
+				?>
+				<DIV class="col-4 menuItem"><A href="?account=<?php print"$account";?>&menu=unfollow">Unfollow</A></DIV>
+				</div>
+				<?php
+			}
+			else{
+				?>
+				<DIV class="col-4 menuItem"><A href="?account=<?php print"$account";?>&menu=follow">Follow</A></DIV>
+				</div>
+				<?php
+			}
+		}
+		
+		
+		genPosts($db, $pid);
 		?>
 	</div>
 
