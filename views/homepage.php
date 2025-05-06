8a2603f76fef1fe963fbf7ba1c898a471b135967
connorwhitlow <158392207+connorwhitlow@users.noreply.github.com>
2025-05-06 14:14:59 -0400

    Update homepage.php

--- views/homepage.php
+++ views/homepage.php
@@ -1,48 +1,44 @@
 <?php
 session_start();
 
-var_dump($_SESSION);
-
 if(!$_SESSION["valid"]){
     header("Location:" . "login.php");
 }
 
 include_once("../whitco_util.php"); 
 include_once("../casibe_util.php"); 
-include_once("../dbproj_connect.php"); 
+include_once("../db_connect.php"); 
 include_once("../bootstrap.php"); 
-
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
         <title>Fuerza Home</title>
+        <h1>
+            <style>h1 {text-align: center;}</style>
+            Welcome, <?php echo $_SESSION["uname"]; ?>!
+        </h1>
     </head>
     
     <body>
-
-        <?php genHeader(); ?>
-
-	    <div class="main-content">
-
-            <h1>
-                <style>h1 {text-align: center;}</style>
-                Welcome <?php echo $_SESSION["uname"]; ?>
-            </h1>
-            
-            <?php
-                genSearchBar();
+        <?php
+            genHeader();
+            genSearchBar(); 
+            if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['explore'])) {
+                    genHomeToggle();
+                    genExplorePage($db);
+            }   
+            else{
+                genExploreToggle();
+                genHomepagePosts($db); 
                 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                    //var_dump($_SERVER);
-                    if($_SERVER["QUERY_STRING"] == 'search'){
-                        //var_dump($_POST);
+                    //if($_SERVER["QUERY_STRING"] == 'search'){
                         processSearch($db, $_POST);
-                    }
-                } 
-                genHomepagePosts($db);
-                genFooter();
-            ?>
-        </div>
+                    //}
+                }    
+            }    
+            genFooter();
+        ?>
     </body>
 </html>
